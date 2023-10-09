4714282b7e6f5e37440874af48e96445edb47a65
gnovotny <g.novotny@gmx.ch>
2023-10-09 19:57:33 +0200

    l

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -39,7 +39,7 @@ const Layout: FC<PropsWithChildren<LayoutProps>> = ({ children, pageProps }) => 
       <div
         className={clsx(
           'flex flex-col lg:flex-row-reverse -lg:items-center justify-between w-full overflow-hidden h-screen min-h-screen max-h-screen',
-          'h-[100svh] min-h-[100svh] max-h-[100svh]'
+          '!h-[100svh] !min-h-[100svh] !max-h-[100svh]'
         )}
       >
         <Bg />
