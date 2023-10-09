5ce5ac3f2f02e2dc03fed35d2965eb1fbb61fbd5
gnovotny <g.novotny@gmx.ch>
2023-10-09 19:55:37 +0200

    l

--- src/components/common/contact-button/index.tsx
+++ src/components/common/contact-button/index.tsx
@@ -1,10 +1,10 @@
 import type { FunctionComponent } from 'react'
 
 import clsx from 'clsx'
+import Link from 'next/link'
 
 import ArrowSVG from '@/components/icons/arrow-right-short.svg'
 import { Button } from '@/components/ui'
-import Link from 'next/link'
 
 interface ContactButtonProps {
   className?: string
@@ -14,7 +14,7 @@ const ContactButton: FunctionComponent<ContactButtonProps> = ({ className }) => 
   return (
     <Link href='/contact'>
       <Button className={clsx('px-5', className)}>
-        Contact <ArrowSVG className='w-4 h-auto pl-1 inline-block' />
+        Contact <ArrowSVG className='inline-block w-4 h-auto pl-1' />
       </Button>
     </Link>
   )
