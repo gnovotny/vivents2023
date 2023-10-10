27f4ca337d45f2ba6ef381a6271aa27bbbe9b7ed
gnovotny <g.novotny@gmx.ch>
2023-10-10 16:08:30 +0200

    l

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const IndexPage = ({ pageProps }: AppProps) => {
   return (
     <div
-      className={clsx('lg:hidden')}
+      className={clsx('lg:hidden pt-6')}
     >
       <IntroProse />
     </div>
