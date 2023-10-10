8c6fbde1e5b5e40d42ddc9ffcfb9b15cc495f6b2
gnovotny <g.novotny@gmx.ch>
2023-10-10 13:34:14 +0200

    l

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,7 +1,10 @@
+import React from 'react'
+
 import { AppProps } from 'next/app'
 
 import ContactButton from '@/components/common/contact-button'
 import { Prose } from '@/components/ui'
+import Title from '@/components/ui/title'
 
 const AppPage = ({ pageProps }: AppProps) => {
   return (
@@ -12,31 +15,34 @@ const AppPage = ({ pageProps }: AppProps) => {
 
 VIVENTS empowers social interactions and offers a suite of tools that increase operational transparency and efficiency while scaling your community. We're not just another platform; we're redefining value exchange and access.`}
         />
-        <div className='flex flex-row items-center gap-2 pt-common'>
-          <a
-            href='https://apps.apple.com/us/app/vivents/id1598350812'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='cursor-pointer'
-          >
-            <img
-              src='/images/app/apple-store.svg'
-              alt='apple-store'
-              className='lg:w-[150px]'
-            />
-          </a>
-          <a
-            href='https://play.google.com/store/apps/details?id=ai.artdeal'
-            className='cursor-pointer'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <img
-              src='/images/app/google-store.svg'
-              alt='google-store'
-              className='lg:w-[150px]'
-            />
-          </a>
+        <div className='flex flex-col gap-3 pt-common'>
+          <Title>Available on</Title>
+          <div className='flex flex-row items-center gap-2 '>
+            <a
+              href='https://apps.apple.com/us/app/vivents/id1598350812'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='cursor-pointer'
+            >
+              <img
+                src='/images/app/apple-store.svg'
+                alt='apple-store'
+                className='lg:w-[150px]'
+              />
+            </a>
+            <a
+              href='https://play.google.com/store/apps/details?id=ai.artdeal'
+              className='cursor-pointer'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                src='/images/app/google-store.svg'
+                alt='google-store'
+                className='lg:w-[150px]'
+              />
+            </a>
+          </div>
         </div>
       </div>
       <ContactButton className='hidden lg:block' />
